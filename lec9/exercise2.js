0ef6b1da9ac955a7662c9900e1b1f2af94b64099
AvoJetts <petertrinh1265@gmail.com>
2024-11-17 20:15:09 -0500

    update

---
+++ lec9/exercise2.js
@@ -0,0 +1,6 @@
+var Start = new Date();
+var age = prompt("What is your age?")
+var currentYear = Start.getFullYear();
+var birthYear = (currentYear - age);
+
+document.write(birthYear);
---
+++ lec9/exercise3.js
@@ -0,0 +1,5 @@
+function addition(){
+  var num1 = Number(document.getElementById('number1').value);
+  var num2 = Number(document.getElementById('number2').value);
+  document.getElementById('Output').innerHTML = num1 + num2;
+}
